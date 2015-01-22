f205c8451817fd0f93da6db7c6451fe63982a9a2
Jan Michael Auer <jan.auer@me.com>
2015-01-21 21:21:12 -0800

    Rename the NC generator to iam_online.
    This is a preparation for the following offline IAM tests.

--- build_nc.sh
+++ build_nc.sh
@@ -1,10 +1,10 @@
 set -e
 
-./english_online.py $1
-cat header.cdl seqTags.cdl labels.cdl wordTargetStrings.cdl targetStrings.cdl seqLengths.cdl seqDims.cdl inputs.cdl > $1.cdl
-echo '}' >> $1.cdl
+./iam_${1}.py $2
+cat header.cdl seqTags.cdl labels.cdl wordTargetStrings.cdl targetStrings.cdl seqLengths.cdl seqDims.cdl inputs.cdl > $2.cdl
+echo '}' >> $2.cdl
 
-echo "Generating $1.nc"
-ncgen -o $1.nc $1.cdl
+echo "Generating $2.nc"
+ncgen -o $2.nc $2.cdl
 rm ./*.cdl
 
