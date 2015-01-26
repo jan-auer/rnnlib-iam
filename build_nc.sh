7a94eaeacd2fde8fc2f917a2b5fdd5fa926a0598
Jan Michael Auer <jan.auer@me.com>
2015-01-25 19:44:06 -0800

    Refactor build_nc.sh

--- build_nc.sh
+++ build_nc.sh
@@ -1,10 +1,26 @@
 set -e
 
-./iam_${1}.py $2
-cat header.cdl seqTags.cdl labels.cdl wordTargetStrings.cdl targetStrings.cdl seqLengths.cdl seqDims.cdl inputs.cdl > $2.cdl
-echo '}' >> $2.cdl
+if [ $# -ne 2 ]; then
+	echo "Usage: ./$FILE <mode> <dataset>"
+	exit
+fi
+
+CDL_FILES=(
+	header.cdl
+	seqTags.cdl
+	labels.cdl
+	wordTargetStrings.cdl
+	targetStrings.cdl
+	seqLengths.cdl
+	seqDims.cdl
+	inputs.cdl
+)
+
+./iam_$1.py $2
+cat ${CDL_FILES[@]} > combined.cdl
+echo '}' >> combined.cdl
 
 echo "Generating $2.nc"
-ncgen -o $2.nc $2.cdl
-rm ./*.cdl
+ncgen -o $2.nc combined.cdl
+rm ${CDL_FILES[@]} combined.cdl
 
